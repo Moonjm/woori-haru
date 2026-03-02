e12f9ddd651ea291e8accea202ef1c8bddfef88d
jm <mym1207@naver.com>
2026-03-02 15:56:30 +0900

    init: SwiftUI 프로젝트 초기 템플릿

---
+++ WooriHaru/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("우리 하루")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
